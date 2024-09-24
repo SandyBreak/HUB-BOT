2bfffd1e0b228ef45165554af9b1bbe5bdb2ed45
VSS <sandybreak3405@gmail.com>
2024-09-24 17:20:21 +0300

    BUGFIX

--- src/routers/main_router.py
+++ src/routers/main_router.py
@@ -74,7 +74,7 @@ async def catch_message(message: Message, bot: Bot) -> None:
         Если сообщение написано не в группе и у пользователя есть номер его темы
         '''
         if (message.chat.id != SUPER_GROUP_ID):
-            #try:
+            try:
             #    '''
             #    Изменяем имя темы если у пользователя изменились имя и фамилия и обновляем сообщение с информацией о пользователе
             #    '''
@@ -90,34 +90,34 @@ async def catch_message(message: Message, bot: Bot) -> None:
                 
                 await bot.copy_message(chat_id=SUPER_GROUP_ID, from_chat_id=message.chat.id, message_id=message.message_id, message_thread_id=id_topic_chat, protect_content=None)
             
-            #except TelegramBadRequest as e:
-            #    '''
-            #    Если тема была удалена
-            #    '''
-            #    if 'message thread not found' in str(e):
-            #        logging.error(f'Message thread not found re-creating the topic...')
-            #        try:
-            #            '''
-            #            Создаем новую тему
-            #            '''
-            #            new_topic = await bot.create_forum_topic(chat_id=SUPER_GROUP_ID, name=message.from_user.full_name)
-            #            await GroupService.save_user_message_thread_id(message.from_user.id, new_topic.message_thread_id)
-            #        except TelegramBadRequest as e:
-            #            if 'not enough rights' in str(e):
-            #                    logging.error(f'Not enough rights!')
-            #                    await bot.send_message(chat_id=SUPER_GROUP_ID, text='Назначьте меня администратором!')
-            #                    return
-            #        except Exception as e:
-            #            logging.error(f'Error create forum topic: {e}')
-            #            
-            #        '''
-            #        Отправляем сообщение с информацией о пользователе и закрепляем его
-            #        '''
-            #        new_user_message = await bot.send_message(chat_id=SUPER_GROUP_ID, text=f'ID пользователя: {message.from_user.id}\nИмя пользователя: {message.from_user.full_name}\nАдрес пользователя: @{message.from_user.username}', reply_to_message_id=new_topic.message_thread_id)
-            #        await bot.pin_chat_message(chat_id=SUPER_GROUP_ID, message_id=new_user_message.message_id)
-            #        await bot.copy_message(chat_id=SUPER_GROUP_ID, from_chat_id=message.chat.id, message_id=message.message_id, message_thread_id=new_topic.message_thread_id, protect_content=None)
-            #    else:
-            #        logging.error(f'Unknown error: {e}')
+            except TelegramBadRequest as e:
+                '''
+                Если тема была удалена
+                '''
+                if 'message thread not found' in str(e):
+                    logging.error(f'Message thread not found re-creating the topic...')
+                    try:
+                        '''
+                        Создаем новую тему
+                        '''
+                        new_topic = await bot.create_forum_topic(chat_id=SUPER_GROUP_ID, name=message.from_user.full_name)
+                        await GroupService.save_user_message_thread_id(message.from_user.id, new_topic.message_thread_id)
+                    except TelegramBadRequest as e:
+                        if 'not enough rights' in str(e):
+                                logging.error(f'Not enough rights!')
+                                await bot.send_message(chat_id=SUPER_GROUP_ID, text='Назначьте меня администратором!')
+                                return
+                    except Exception as e:
+                        logging.error(f'Error create forum topic: {e}')
+                        
+                    '''
+                    Отправляем сообщение с информацией о пользователе и закрепляем его
+                    '''
+                    new_user_message = await bot.send_message(chat_id=SUPER_GROUP_ID, text=f'ID пользователя: {message.from_user.id}\nИмя пользователя: {message.from_user.full_name}\nАдрес пользователя: @{message.from_user.username}', reply_to_message_id=new_topic.message_thread_id)
+                    await bot.pin_chat_message(chat_id=SUPER_GROUP_ID, message_id=new_user_message.message_id)
+                    await bot.copy_message(chat_id=SUPER_GROUP_ID, from_chat_id=message.chat.id, message_id=message.message_id, message_thread_id=new_topic.message_thread_id, protect_content=None)
+                else:
+                    logging.error(f'Unknown error: {e}')
         elif message.is_topic_message and message.from_user.id != bot_data.id:
             '''
             Если сообщение написано в одной из тем и это сообщение принадлежит не боту
